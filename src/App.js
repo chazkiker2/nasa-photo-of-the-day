6ea1971463e8105ebd0b718d02111a0c5bb30a8a
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-07 17:41:40 -0400

    add import statements

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {API_KEY, BASE_URL} from "./constants/index";
 import "./App.css";
 
+console.log(axios);
+
 function App() {
+
+
   return (
     <div className="App">
       <p>
---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://api.nasa.gov/planetary/apod";
+export const API_KEY = "api_key=DEMO_KEY";
+
+// https://api.nasa.gov/planetary/apod\?api_key\=DEMO_KEY\&date\=1995-06-16\?api_key\=DEMO_KEY\&date\=1995-06-16
