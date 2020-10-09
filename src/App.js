61c141b708a565327556d36801aebf2aa5d4b0d7
chaz-kiker <chaz-kiker@lambdastudents.com>
2020-10-08 21:44:53 -0400

    clean up code for readability

--- src/App.js
+++ src/App.js
@@ -82,18 +82,6 @@ const App = () => {
 		setDate(randomDateString);
 	};
 
-	// const DateForm = () => {
-	// 	return (
-	// 		<form onSubmit={handleSubmit}>
-	// 			<label>
-	// 				Date:
-	// 				<input type="text" {...bind} />
-	// 			</label>
-	// 			<input type="submit" value="Submit" />
-	// 		</form>
-	// 	);
-	// };
-
 	useEffect(() => {
 		const endpoint = `&date=${date}`
 		const queryString = `${BASE_URL}?${API_KEY}${endpoint}`;
@@ -126,59 +114,3 @@ const App = () => {
 export default App;
 
 
-
-
-
-	// const handleChange = (e) => {
-	// 	console.log(e);
-	// 	console.log(e.target);
-	// 	debugger;
-	// 	e.preventDefault();
-	// 	const { value } = e.currentTarget;
-	// 	setInputValue(value);
-
-	// };
-
-	// const getRandomInterval = (min, max) => {
-	// 	return Math.floor(Math.random() * (max - min + 1) + min);
-	// };
-
-	// const handleRandom = (e) => {
-	// 	const randYear = getRandomInterval(1995, 2020);
-	// 	const randMonth = (randYear === 1995)
-	// 		? getRandomInterval(6, 12)
-	// 		: getRandomInterval(1, 12);
-	// 	const getRandomDay = () => {
-	// 		const getMaxDay = () => {
-	// 			if (randMonth === 2) {
-	// 				return 28;
-	// 			} else if (randMonth === 4 || 6 || 9 || 11) {
-	// 				return 30;
-	// 			} else {
-	// 				return 31;
-	// 			}
-	// 			//31 : 1, 3, 5, 7, 8, 10, 12 || 30 : 4, 6, 9, 11 || 28 : 2
-	// 		}
-	// 		return getRandomInterval(((randYear === 1995) ? 16 : 1), getMaxDay())
-	// 	}
-	// 	const randDay = getRandomDay();
-	// 	const randomDateString = `${String(randYear)}-${String(randMonth).padStart(2, '0')}-${String(randDay).padStart(2, '0')}`;
-	// 	setDate(randomDateString);
-	// };
-
-	// const handleSubmit = (e) => {
-	// 	setDate(inputValue);
-	// 	setInputValue(inputValue);
-	// 	e.preventDefault();
-	// }
-
-
-		// const styleFooter = {
-	// 	display: "flex",
-	// 	justifyContent: "center",
-	// 	alignItems: "center",
-	// 	backgroundColor: "black",
-	// };
-	// const styleH4 = {
-	// 	color: "white",
-	// };
--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -24,7 +24,6 @@ const StyledDateSection = styled.div`
 	}
 `;
 
-
 const DateInput = (props) => {
 	const [isOpen, setIsOpen] = useState(false);
 	const { bind, handleSubmit, handleRandom } = props;
@@ -44,63 +43,3 @@ const DateInput = (props) => {
 }
 
 export default DateInput;
-
-
-		// <div className="date-container" style={styleDiv}>
-		// 	<h5>Search for Date: (YYYY-MM-DD)</h5>
-		// 	<input type="text" value={inputValue} onChange={handleChange} />
-		// 	<Button color="secondary" onClick={handleSubmit}>Submit</Button>
-		// </div>
-		// <>
-		// 	<Button onClick={toggle}>Search</Button>
-			
-		// 	<Collapse isOpen={isOpen}>
-		// 		{/* <FormGroup style={{ display: "flex", flexFlow: "column nowrap", justifyContent: "center", textAlign: "center", alignItems: "center" }}> */}
-		// 			<Label for="searchDate">Search for Date</Label>
-		// 			<Input id="searchDate" type="text" placeholder="YYYY-MM-DD" value={inputValue} onInput={handleChange} onChange={handleSubmit}  />
-		// 			<Button onClick={handleSubmit}>Submit</Button>
-		// 		{/* </FormGroup> */}
-		// 	</Collapse>
-		// 	<Button onClick={handleRandom}>Random</Button>
-		// </>
-
-// import React, { useState } from "react";
-// // import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Button, Collapse, FormGroup, Label, Input, } from "reactstrap";
-
-// const DateInput = (props) => {
-// 	const { handleChange, inputValue, handleSubmit, handleRandom } = props;
-// 	const [isOpen, setIsOpen] = useState(false);
-
-// 	const toggle = () => setIsOpen(!isOpen);
-
-// 	const styleDiv = {
-// 		display: "flex",
-// 		flexFlow: "column nowrap",
-// 		justifyContent: "center",
-// 		alignItems: "center",
-// 	};
-
-// 	return (
-// 		// <div className="date-container" style={styleDiv}>
-// 		// 	<h5>Search for Date: (YYYY-MM-DD)</h5>
-// 		// 	<input type="text" value={inputValue} onChange={handleChange} />
-// 		// 	<Button color="secondary" onClick={handleSubmit}>Submit</Button>
-// 		// </div>
-// 		<div style={styleDiv}>
-// 			<Button color="primary" size="lg" onClick={toggle} style={{ margin: "2rem", }}>Search</Button>
-			
-// 			<Collapse isOpen={isOpen}>
-// 				<FormGroup style={{ display: "flex", flexFlow: "column nowrap", justifyContent: "center", textAlign: "center", alignItems: "center" }}>
-// 					<Label for="searchDate">Search for Date</Label>
-// 					<Input type="text" name="date" id="searchDate" placeholder="YYYY-MM-DD" onChange={handleChange} inputvalue={inputValue} style={{ textAlign: "center" }} />
-// 					<Button color="secondary" onClick={handleSubmit} style={{ marginTop: "1rem" }}>Submit</Button>
-// 				</FormGroup>
-// 			</Collapse>
-// 			<Button color="primary" size="lg" onClick={handleRandom} style={{margin: "2rem", }}>Random</Button>
-// 		</div>
-
-// 	);
-// }
-
-// export default DateInput;
